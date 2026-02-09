e81738755b04f07d0bb438fb0f46316df7caf7ee
Gabrick75 <gabriel.dijair.souza@gmail.com>
2026-02-09 19:59:42 -0300

    chore: add git configuration files

--- src/main/java/com/gabrick/ticket_system/TicketSystemApplication.java
+++ src/main/java/com/gabrick/ticket_system/TicketSystemApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TicketSystemApplication {
-
+//a
 	public static void main(String[] args) {
 		SpringApplication.run(TicketSystemApplication.class, args);
 	}
